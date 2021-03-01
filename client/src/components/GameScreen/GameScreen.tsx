03601e45764207f4f3e00e2ec121bcbb775cefc2
AnAtoliyAK <aliaksandrau89@gmail.com>
2021-03-01 18:55:11 +0300

    fix: change classic styles

--- client/src/components/GameScreen/GameScreen.tsx
+++ client/src/components/GameScreen/GameScreen.tsx
@@ -6,6 +6,7 @@ import { FullScreen, useFullScreenHandle } from 'react-full-screen';
 import FullscreenIcon from '@material-ui/icons/Fullscreen';
 import VolumeMuteIcon from '@material-ui/icons/VolumeMute';
 import VolumeOffIcon from '@material-ui/icons/VolumeOff';
+import BackspaceIcon from '@material-ui/icons/Backspace';
 import { Button } from '@material-ui/core';
 import { useStore } from '../../hooks/hooks';
 import { DEFAULT_FOREIGN_LANGUAGE, WORDS_CONFIG } from '../../constants';
@@ -13,6 +14,7 @@ import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/src/styles.scss';
 import gameMusic from '../../assets/sounds/music.mp3';
 import { FIELD_SIZES } from '../../constants';
+import { NavLink } from 'react-router-dom';
 
 const GameScreen: React.FC = () => {
     const gameSettingsStore = useStore('gameSettingsStore');
@@ -21,6 +23,13 @@ const GameScreen: React.FC = () => {
     return (
         <div className="GameScreen">
             <div className="game-controls">
+                <NavLink to="/main" style={{ textDecoration: 'none' }}>
+                    <Button variant="contained" color="primary" size="small" startIcon={<BackspaceIcon />}>
+                        {gameSettingsStore.gameSettings.gameLanguage === DEFAULT_FOREIGN_LANGUAGE
+                            ? WORDS_CONFIG.BACK_BUTTON.foreign
+                            : WORDS_CONFIG.BACK_BUTTON.native}
+                    </Button>
+                </NavLink>
                 <Button
                     variant="contained"
                     color="primary"
